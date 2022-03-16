67ed7246ce92751ff9c0c84f8871e2b0931754cf
harsh <harsh@pop-os.localdomain>
2022-03-16 16:09:49 +0530

    :sparkles: Deposit Event & function definations added

--- contracts/LenderPool/LenderPool.sol
+++ contracts/LenderPool/LenderPool.sol
@@ -7,15 +7,24 @@ import "hardhat/console.sol";
 
 contract LenderPool is ILenderPool {
     /**
+     * @dev See {ILenderPool-deposit}
      *
+     * Requirements:
+     *
+     * - `lendingAmount` should be greater than zero
+     * - `lendingAmount` must be approved from the Tether contract for the LenderPool contact
+     *
+     * Emits {Deposit} event
      */
     function deposit(address tetherAddress, uint lendingAmount)
         external
         payable
     {
+        require(lendingAmount > 0, "Lending amount invalid");
         ITetherToken _tether = ITetherToken(tetherAddress);
         uint allowance = _tether.allowance(msg.sender, address(this));
         require(allowance >= lendingAmount, "Amount not approved");
         _tether.transferFrom(msg.sender, address(this), lendingAmount);
+        emit Deposit(msg.sender, lendingAmount);
     }
 }
--- contracts/LenderPool/interface/ILenderPool.sol
+++ contracts/LenderPool/interface/ILenderPool.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.12;
 
 interface ILenderPool {
-    /*
-     *
+    /**
+     * @dev Emitted when funds are deposited by the `lenderAddress`.
+     * `lendingAmount` is the total amount added
+     */
+    event Deposit(address lenderAddress, uint lendingAmount);
+
+    /**
+     * @dev Transfer Tether token from `msg.sender` address to contract
+     * @param tetherAddress, address to the stable token. (It should be constant in production)
+     * @param lendingAmount, total amount that the `user` transfers to the lending pool
      */
     function deposit(address tetherAddress, uint lendingAmount)
         external
