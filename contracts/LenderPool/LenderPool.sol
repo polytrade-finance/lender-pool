0acf6f8837ef422dc4b1915cec19d3705c785810
harsh <harsh@pop-os.localdomain>
2022-03-17 14:01:06 +0530

    :sparkles: safe transfer, balance, getBalance added

--- contracts/LenderPool/LenderPool.sol
+++ contracts/LenderPool/LenderPool.sol
@@ -1,30 +1,46 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "./interface/ITetherToken.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interface/ILenderPool.sol";
-import "hardhat/console.sol";
 
+/**
+ * @author Polytrade
+ * @title LenderPool V2
+ */
 contract LenderPool is ILenderPool {
+    using SafeERC20 for IERC20;
+    /// @notice
+    address public tokenAddress;
+    mapping(address => uint) private _balance;
+
+    constructor(address _tokenAddress) {
+        tokenAddress = _tokenAddress;
+    }
+
     /**
-     * @dev See {ILenderPool-deposit}
-     *
+     * @notice Deposit token to smart contract
+     * @dev Transfers the approved token from msg.sender to lender pool
+     * @param amount The number of tokens user wants to transfer
      * Requirements:
      *
      * - `lendingAmount` should be greater than zero
-     * - `lendingAmount` must be approved from the Tether contract for the LenderPool contact
+     * - `lendingAmount` must be approved from the token contract for the LenderPool contact
      *
      * Emits {Deposit} event
      */
-    function deposit(address tetherAddress, uint lendingAmount)
-        external
-        payable
-    {
-        require(lendingAmount > 0, "Lending amount invalid");
-        ITetherToken _tether = ITetherToken(tetherAddress);
-        uint allowance = _tether.allowance(msg.sender, address(this));
-        require(allowance >= lendingAmount, "Amount not approved");
-        _tether.transferFrom(msg.sender, address(this), lendingAmount);
-        emit Deposit(msg.sender, lendingAmount);
+    function deposit(uint amount) external {
+        require(amount > 0, "Lending amount invalid");
+        IERC20 _token = IERC20(tokenAddress);
+        uint allowance = _token.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Amount not approved");
+        _token.safeTransferFrom(msg.sender, address(this), amount);
+        _balance[msg.sender] += amount;
+        emit Deposit(msg.sender, amount);
+    }
+
+    function getBalance(address lender) external view returns (uint) {
+        return _balance[lender];
     }
 }
--- contracts/LenderPool/interface/ILenderPool.sol
+++ contracts/LenderPool/interface/ILenderPool.sol
@@ -3,17 +3,23 @@ pragma solidity ^0.8.12;
 
 interface ILenderPool {
     /**
+     * @notice Emits when new fund is added to Lender Pool
      * @dev Emitted when funds are deposited by the `lenderAddress`.
      * `lendingAmount` is the total amount added
      */
-    event Deposit(address indexed lenderAddress, uint indexed lendingAmount);
+    event Deposit(address indexed lenderAddress, uint lendingAmount);
 
     /**
-     * @dev Transfer Tether token from `msg.sender` address to contract
-     * @param tetherAddress, address to the stable token. (It should be constant in production)
-     * @param lendingAmount, total amount that the `user` transfers to the lending pool
+     * @notice Deposit token to smart contract
+     * @dev Transfers the approved token from msg.sender to lender pool
+     * @param amount The number of tokens user wants to transfer
      */
-    function deposit(address tetherAddress, uint lendingAmount)
-        external
-        payable;
+    function deposit(uint amount) external;
+
+    /**
+     * @notice returns the total amount lent by lender
+     * @param lender address of lender
+     * @return returns the balance of lender
+     */
+    function getBalance(address lender) external view returns (uint);
 }
