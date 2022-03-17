1208e0e2de1f36aec9d5fb87bea079d31a0764b2
harsh <harsh@pop-os.localdomain>
2022-03-17 14:04:51 +0530

    :white_check_mark: test added for 100% coverage and tether replaced by token

--- test/LenderPool.test.ts
+++ test/LenderPool.test.ts
@@ -1,55 +1,65 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { TetherToken, LenderPool } from "../typechain";
+import { Token, LenderPool } from "../typechain";
 
 describe("LenderPool", function () {
   let accounts: SignerWithAddress[];
   let addresses: string[];
   let lenderPool: LenderPool;
-  let tetherToken: TetherToken;
+  let token: Token;
 
   before(async () => {
     accounts = await ethers.getSigners();
     addresses = accounts.map((account: SignerWithAddress) => account.address);
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy("Tether", "USDC", 6);
+    await token.deployed();
     const LenderPool = await ethers.getContractFactory("LenderPool");
-    lenderPool = await LenderPool.deploy();
+    lenderPool = await LenderPool.deploy(token.address);
     await lenderPool.deployed();
-    const TetherToken = await ethers.getContractFactory("TetherToken");
-    tetherToken = await TetherToken.deploy();
-    await tetherToken.deployed();
   });
 
   it("Should deploy contracts successfully", async function () {
     expect(
       await ethers.provider.getCode(lenderPool.address)
     ).to.be.length.above(10);
-    expect(
-      await ethers.provider.getCode(tetherToken.address)
-    ).to.be.length.above(10);
+    expect(await ethers.provider.getCode(token.address)).to.be.length.above(10);
   });
 
   it("should approve token", async function () {
-    await tetherToken.connect(accounts[0]).approve(lenderPool.address, 100);
+    await token.connect(accounts[0]).approve(lenderPool.address, 100);
     expect(
       ethers.BigNumber.from("100").eq(
-        await tetherToken.allowance(addresses[0], lenderPool.address)
+        await token.allowance(addresses[0], lenderPool.address)
       )
-    ).to.be.true;
+    );
+  });
+
+  it("balance of user should be zero initially", async function () {
+    expect(await lenderPool.getBalance(addresses[0])).to.be.equal(0);
   });
 
   it("amount should be deposited after approval", async function () {
-    await lenderPool.connect(accounts[0]).deposit(tetherToken.address, 100);
+    await lenderPool.connect(accounts[0]).deposit(100);
     expect(
-      ethers.BigNumber.from("100").eq(
-        await tetherToken.balanceOf(lenderPool.address)
-      )
-    ).to.be.true;
+      ethers.BigNumber.from("100").eq(await token.balanceOf(lenderPool.address))
+    );
+  });
+
+  it("balance of user should be increase after deposit", async function () {
+    expect(await lenderPool.getBalance(addresses[0])).to.be.equal(100);
   });
 
   it("deposit should return if allowance is less than lending amount", async function () {
-    expect(
-      lenderPool.connect(accounts[0]).deposit(tetherToken.address, 100)
-    ).to.be.revertedWith("Amount not approved");
+    expect(lenderPool.connect(accounts[0]).deposit(100)).to.be.revertedWith(
+      "Amount not approved"
+    );
+  });
+
+  it("deposit should return if amount is zero", async function () {
+    expect(lenderPool.connect(accounts[0]).deposit(0)).to.be.revertedWith(
+      "Lending amount invalid"
+    );
   });
 });
