e10999d73fdda1451c46e1887a8aaa7df98464d8
harsh <harsh@pop-os.localdomain>
2022-03-17 19:57:19 +0530

    :art: NatSpec changes

--- contracts/LenderPool/LenderPool.sol
+++ contracts/LenderPool/LenderPool.sol
@@ -23,6 +23,7 @@ contract LenderPool is ILenderPool {
      * @notice Deposit token to smart contract
      * @dev Transfers the approved token from msg.sender to lender pool
      * @param amount, the number of tokens to be lent
+     *
      * Requirements:
      *
      * - `lendingAmount` should be greater than zero
