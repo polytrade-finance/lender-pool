9e051ac5ae891fd041c259930cafae5ab175b0cb
harsh <harsh@pop-os.localdomain>
2022-04-08 15:33:36 +0530

    :passport_control: add minter and burner

--- contracts/Token/Token.sol
+++ contracts/Token/Token.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Token is ERC20 {
+contract Token is ERC20, AccessControl {
     uint8 private immutable _decimals;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     constructor(
         string memory name_,
@@ -13,6 +16,16 @@ contract Token is ERC20 {
     ) ERC20(name_, symbol_) {
         _decimals = decimals_;
         _mint(msg.sender, 1_000_000_000 * (10**decimals_));
+    }
+
+    function mint(address to, uint amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint amount) public onlyRole(BURNER_ROLE) {
+        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
+        _burn(from, amount);
     }
 
     function decimals() public view virtual override returns (uint8) {
