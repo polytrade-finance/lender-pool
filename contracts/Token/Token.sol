1b39823c0ed03a7569c3f9d5cacccd6c846c6006
harsh <harsh@pop-os.localdomain>
2022-04-08 16:46:48 +0530

    :passport_control:  burner role modified

--- contracts/Token/Token.sol
+++ contracts/Token/Token.sol
@@ -3,29 +3,25 @@ pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Token is ERC20, AccessControl {
+contract Token is ERC20, ERC20Burnable, AccessControl {
     uint8 private immutable _decimals;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     constructor(
         string memory name_,
         string memory symbol_,
         uint8 decimals_
     ) ERC20(name_, symbol_) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
         _decimals = decimals_;
         _mint(msg.sender, 1_000_000_000 * (10**decimals_));
     }
 
-    function mint(address to, uint amount) public {
-        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
-    }
-
-    function burn(address from, uint amount) public onlyRole(BURNER_ROLE) {
-        require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
-        _burn(from, amount);
     }
 
     function decimals() public view virtual override returns (uint8) {
