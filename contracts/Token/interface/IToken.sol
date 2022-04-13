5d4b28eadeb024638d0e84b98bd0b96bf9c830e8
harsh <harsh@pop-os.localdomain>
2022-04-13 15:45:30 +0530

    :sparkles: interface added for token

---
+++ contracts/Token/interface/IToken.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IToken is IERC20 {
+    function mint(address to, uint amount) external;
+
+    function setMinter(address minter) external;
+}
