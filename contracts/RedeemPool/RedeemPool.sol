3a3476dd0030a00f0f42bf97c91b55a4ab5f1ef6
harsh <harsh@pop-os.localdomain>
2022-04-18 09:45:23 +0530

    :memo: NatSpec added for Redeem Pool

--- contracts/RedeemPool/RedeemPool.sol
+++ contracts/RedeemPool/RedeemPool.sol
@@ -5,6 +5,10 @@ import "./interface/IRedeemPool.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../Token/interface/IToken.sol";
 
+/**
+ * @author Polytrade
+ * @title RedeemPool 
+ */
 contract RedeemPool is IRedeemPool {
     using SafeERC20 for IToken;
 
@@ -17,12 +21,19 @@ contract RedeemPool is IRedeemPool {
     }
 
     /**
-    * @notice
-    * @dev
-    * @param amount
-    */
+     * @notice Deposit stable token to smart contract
+     * @dev Transfers the approved stable token from msg.sender to redeem pool
+     * @param amount, the number of stable token deposited
+     *
+     * Requirements:
+     *
+     * - `amount` should be greater than zero
+     * - `amount` must be approved from the stable token contract for the RedeemPool contact
+     *
+     * Emits {StableDeposited} event    
+     */
     function depositStable(uint amount) external {
-        require(amount > 0, "Invalid amount");
+        require(amount > 0, "Lending amount is 0");
         uint allowance = stable.allowance(msg.sender, address(this));
         require(allowance >= amount, "Not enough allowance");
         stable.safeTransferFrom(msg.sender, address(this), amount);
@@ -30,11 +41,21 @@ contract RedeemPool is IRedeemPool {
     }
 
     /**
-    * @notice
-    * @dev
-    * @param amount
-    */
+     * @notice exchange tStable token for the stable token 
+     * @dev Transfers the approved tStable token from msg.sender to redeem pool and burn it
+     * @dev Transfers the  equivalent amount of stable token from redeem pool to msg.sender
+     * @param amount, the number of tokens to be exchanged
+     *
+     * Requirements:
+     *
+     * - `amount` should be greater than zero
+     * - `amount` must be approved from the tStable token contract for the RedeemPool contact
+     * - `amount` must be less than balanceOf stable token of Redeem Pool
+     *
+     * Emits {StableWithdrawn} event    
+     */
     function getStable(uint amount) external {
+        require(amount > 0, "Lending amount is 0");
         uint balance = stable.balanceOf(address(this));
         require(balance >= amount, "insufficient balance in pool");
         uint allowance = tStable.allowance(msg.sender, address(this));
--- contracts/RedeemPool/interface/IRedeemPool.sol
+++ contracts/RedeemPool/interface/IRedeemPool.sol
@@ -2,10 +2,46 @@
 pragma solidity ^0.8.12;
 
 interface IRedeemPool {
+
+    /**
+     * @notice Emits when tStable token is exchanged for stable token
+     * @param amount, the number of tokens exchanged
+     */
     event StableWithdrawn(uint amount);
+
+    /**
+     * @notice Emits when stable token is added to the Redeem Pool
+     * @param amount, the number of stable token deposited
+     */    
     event StableDeposited(uint amount);
 
+    /**
+     * @notice Deposit stable token to smart contract
+     * @dev Transfers the approved stable token from msg.sender to Redeem Pool
+     * @param amount, the number of stable token deposited
+     *
+     * Requirements:
+     *
+     * - `amount` should be greater than zero
+     * - `amount` must be approved from the stable token contract for the RedeemPool contact
+     *
+     * Emits {StableDeposited} event    
+     */
     function depositStable(uint amount) external;
 
+    /**
+     * @notice exchange tStable token for the stable token 
+     * @dev Transfers the approved tStable token from msg.sender to redeem pool and burn it
+     * @dev Transfers the  equivalent amount of stable token from redeem pool to msg.sender
+     * @param amount, the number of tokens to be exchanged
+     *
+     * Requirements:
+     *
+     * - `amount` should be greater than zero
+     * - `amount` must be approved from the tStable token contract for the RedeemPool contact
+     * - `amount` must be less than balanceOf stable token of Redeem Pool
+     *
+     * Emits {StableWithdrawn} event    
+     */
     function getStable(uint amount) external;
 }
