c9e77fcd038e2f3b4223748450bfbfc3b3de5689
harsh <harsh@pop-os.localdomain>
2022-04-19 12:12:29 +0530

    :memo: NatSpec updated and _minter variable removed

--- contracts/Token/Token.sol
+++ contracts/Token/Token.sol
@@ -6,10 +6,14 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "./interface/IToken.sol";
 
+/**
+ * @author Polytrade
+ * @title Token
+ */
 contract Token is IToken, ERC20, ERC20Burnable, AccessControl {
     uint8 private immutable _decimals;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    address private _minter;
+    mapping(address => bool) private _minters;
 
     constructor(
         string memory name_,
@@ -21,19 +25,36 @@ contract Token is IToken, ERC20, ERC20Burnable, AccessControl {
         _mint(msg.sender, 1_000_000_000 * (10**decimals_));
     }
 
+    /**
+     * @notice adds new minter address
+     * @dev grants `MINTER_ROLE` to address `minter`
+     * @param minter, address of the new minter
+     *
+     * Emits a {RoleGranted} event
+     */
     function setMinter(address minter) external {
-        _minter = minter;
-        grantRole(MINTER_ROLE, _minter);
+        grantRole(MINTER_ROLE, minter);
     }
 
+    /**
+     * @notice mints ERC20 token
+     * @dev creates `amount` tokens and assigns them to `to`, increasing the total supply.
+     * @param to, receiver address of the ERC20 address
+     * @param amount, amount of ERC20 token minted
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     */
     function mint(address to, uint amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    function getMinter() external view returns (address) {
-        return _minter;
-    }
-
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     */
     function decimals() public view virtual override returns (uint8) {
         return _decimals;
     }
