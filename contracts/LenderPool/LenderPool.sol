3b0caae055aa2f79095cf2bdd834f9672f499bd9
Adam Boudj <boudjemaa.adam@gmail.com>
2022-04-30 13:27:43 +0400

    :sparkles: add depositLimit

--- contracts/LenderPool/LenderPool.sol
+++ contracts/LenderPool/LenderPool.sol
@@ -7,10 +7,6 @@ import "../RedeemPool/interface/IRedeemPool.sol";
 import "../Verification/interface/IVerification.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./interface/ILenderPool.sol";
-import "../Token/interface/IToken.sol";
-import "../RedeemPool/interface/IRedeemPool.sol";
 
 /**
  * @author Polytrade
@@ -28,6 +24,7 @@ contract LenderPool is ILenderPool, Ownable {
     IVerification public verification;
 
     uint16 public currentRound = 0;
+    uint public depositLimit;
 
     constructor(
         address _stableAddress,
