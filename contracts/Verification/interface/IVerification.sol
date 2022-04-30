a1d3319091400ee5016f6fe494e48aeb199eba25
Adam Boudj <boudjemaa.adam@gmail.com>
2022-04-30 13:29:24 +0400

    :sparkles: add IVerification interface

---
+++ contracts/Verification/interface/IVerification.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.12;
+
+/**
+ * @author Polytrade
+ * @title IVerification
+ */
+interface IVerification {
+    /**
+     * @notice Returns whether a user's KYC is verified or not
+     * @dev returns a boolean if the KYC is valid
+     * @param user, address of the user to check
+     * @return returns true if user's KYC is valid or false if not
+     */
+    function isValid(address user) external view returns (bool);
+}
