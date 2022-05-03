4d1ebc71f66582fc428d835c41dd5a0b18b2547c
harsh <harsh@pop-os.localdomain>
2022-05-03 13:06:15 +0530

    :wrench: hardhat config changes for forking

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.12",
   networks: {
+    hardhat: {
+      forking: {
+        url: process.env.MAINNET_URL || "",
+      },
+      accounts: {
+        accountsBalance: "1000000000000000000000000",
+      },
+      gas: 2100000,
+    },
+    mumbai: {
+      url: process.env.MUMBAI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
