060044383823bce331db954894cac29456b31d48
harsh <harsh@pop-os.localdomain>
2022-05-14 14:51:32 +0530

    :sparkles: interface for trade reward added

---
+++ contracts/TradeReward/interface/ITradeReward.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+interface ITradeReward {
+    struct Lender {
+        uint16 round;
+        uint40 startPeriod;
+        uint pendingRewards;
+        uint deposit;
+    }
+
+    struct RoundInfo {
+        uint16 tradeRate;
+        uint40 startTime;
+        uint40 endTime;
+    }
+
+    function deposit(address lender, uint amount) external;
+
+    function withdraw(address lender, uint amount) external;
+
+    function rewardOf(address lender) external returns (uint);
+
+    function claimReward(address lender, uint amount) external;
+
+    function setTradeRate(uint16 tradeRate) external;
+}
