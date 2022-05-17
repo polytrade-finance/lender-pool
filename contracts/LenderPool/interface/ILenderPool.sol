372921a9e73041240f962ee7c5d8eca57467d72a
harsh-polytrade <harsh@polytreade.finance>
2022-05-17 13:28:26 +0530

    :fire: dead code removed from interface lender pool

--- contracts/LenderPool/interface/ILenderPool.sol
+++ contracts/LenderPool/interface/ILenderPool.sol
@@ -115,14 +115,13 @@ interface ILenderPool {
      * @notice converts all the deposited stable token into tStable token and transfers to the lender
      * @dev calculates the tStable token lender can claim and transfers it to the lender
      */
-    function withdrawAllTStable() external;
-
+    function withdrawAllDeposit() external;
     /**
      * @notice converts the given amount of stable token into tStable token and transfers to lender
      * @dev checks the required condition and converts stable token to tStable and transfers to lender
      * @param amount, total amount of stable token to be converted to tStable token
      */
-    function withdrawTStable(uint amount) external;
+    function withdrawDepsoit(uint amount) external;
 
     /**
      * @notice send lender all the reward
@@ -131,33 +130,6 @@ interface ILenderPool {
      * Emits {Withdraw} event
      */
     function claimRewards() external;
-
-    /**
-     * @notice adds a new round
-     * @dev increment currentRound and adds a new round, only owner can call
-     * @param _rewardAPY, new value of new round.apy
-     *
-     * Emits {NewRewardAPY} event
-     */
-    function setAPY(uint16 _rewardAPY) external;
-
-    /**
-     * @notice transfers user all the reward in stable token
-     * @dev calculates and mint the reward
-     * @dev calls redeemStableTo function from RedeemPool to convert tStable to stable
-     *
-     * Requirements:
-     *
-     * - total reward should be not more than stable tokens in RedeemPool
-     *
-     */
-    function redeemAll() external;
-
-    /**
-     * @notice returns value of APY of current round
-     * @return returns value of APY of current round
-     */
-    function getAPY() external view returns (uint16);
 
     /**
      * @notice returns amount of stable token deposited by the lender
