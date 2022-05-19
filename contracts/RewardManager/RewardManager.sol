0bc5f2544e147f1c93f62db4b66e4d2b4c889530
harsh-polytrade <harsh@polytrade.finance>
2022-05-19 17:28:10 +0530

    :memo: NatSpec RewardManager added

--- contracts/RewardManager/RewardManager.sol
+++ contracts/RewardManager/RewardManager.sol
@@ -5,6 +5,10 @@ import "./interface/IRewardManager.sol";
 import "../Reward/interface/IReward.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+/**
+ * @author Polytrade
+ * @title Reward Manager V2
+ */
 contract RewardManager is IRewardManager, AccessControl {
     IReward public stable;
     IReward public trade;
@@ -17,6 +21,13 @@ contract RewardManager is IRewardManager, AccessControl {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
+    /**
+     * @notice `increaseDeposit` increases the amount deposited by lender.
+     * @dev It calls the `deposit` function of all the rewards in `RewardManager`.
+     * @dev It can by only called by `LenderPool`.
+     * @param lender, address of the lender 
+     * @param amount, amount deposited by the lender
+     */
     function increaseDeposit(address lender, uint amount)
         external
         onlyRole(LENDER_POOL)
@@ -25,6 +36,13 @@ contract RewardManager is IRewardManager, AccessControl {
         stable.deposit(lender, amount);
     }
 
+    /**
+     * @notice `withdrawDeposit` decrease the amount deposited by the lender.
+     * @dev It calls the `withdraw` function of all the rewards in `RewardManager`
+     * @dev It can by only called by `LenderPool`.
+     * @param lender, address of the lender 
+     * @param amount, amount withdrawn by the lender
+     */
     function withdrawDeposit(address lender, uint amount)
         external
         onlyRole(LENDER_POOL)
@@ -33,21 +51,40 @@ contract RewardManager is IRewardManager, AccessControl {
         stable.withdraw(lender, amount);
     }
 
+    /**
+     * @notice `claimRewardsFor` claims reward for the lender.
+     * @dev All the reward are transfered to the lender.
+     * @dev It can by only called by `LenderPool`.
+     * @param lender, address of the lender
+     */
     function claimRewardsFor(address lender) external onlyRole(LENDER_POOL) {
         stable.claimReward(lender);
         trade.claimReward(lender);
     }
 
+    /**
+     * @notice `claimRewardsFor` claims reward for the lender.
+     * @dev All the reward are transfered to the lender.
+     * @dev It can be directly called by lender.
+     */
     function claimRewards() external {
         stable.claimReward(msg.sender);
         trade.claimReward(msg.sender);
     }
 
+    /**
+     * @notice `pauseRewards` sets the reward for all the tokens to 0 
+     */
     function pauseReward() external onlyRole(LENDER_POOL) {
         stable.pauseReward();
         trade.pauseReward();
     }
 
+    /**
+     * @notice `rewardOf` returns array of reward for the lender 
+     * @dev It returns array of number, where each element is a reward
+     * @dev For example - [stable reward, trade reward 1, trade reward 2]
+     */
     function rewardOf(address lender) external view returns (uint[] memory) {
         uint[] memory rewards = new uint[](2);
         rewards[0] = stable.rewardOf(lender);
--- contracts/RewardManager/interface/IRewardManager.sol
+++ contracts/RewardManager/interface/IRewardManager.sol
@@ -2,15 +2,49 @@
 pragma solidity ^0.8.12;
 
 interface IRewardManager {
+
+    /**
+     * @notice `claimRewardsFor` claims reward for the lender.
+     * @dev All the reward are transfered to the lender.
+     * @dev It can by only called by `LenderPool`.
+     * @param lender, address of the lender
+     */
     function claimRewardsFor(address lender) external;
 
+    /**
+     * @notice `claimRewardsFor` claims reward for the lender.
+     * @dev All the reward are transfered to the lender.
+     * @dev It can be directly called by lender.
+     */
     function claimRewards() external;
 
+    /**
+     * @notice `increaseDeposit` increases the amount deposited by lender.
+     * @dev It calls the `deposit` function of all the rewards in `RewardManager`.
+     * @dev It can by only called by `LenderPool`.
+     * @param lender, address of the lender 
+     * @param amount, amount deposited by the lender
+     */
     function increaseDeposit(address lender, uint amount) external;
 
+    /**
+     * @notice `withdrawDeposit` decrease the amount deposited by the lender.
+     * @dev It calls the `withdraw` function of all the rewards in `RewardManager`
+     * @dev It can by only called by `LenderPool`.
+     * @param lender, address of the lender 
+     * @param amount, amount withdrawn by the lender
+     */
     function withdrawDeposit(address lender, uint amount) external;
 
+    /**
+     * @notice `pauseRewards` sets the reward for all the tokens to 0 
+     */
     function pauseReward() external;
 
+    /**
+     * @notice `rewardOf` returns array of reward for the lender 
+     * @dev It returns array of number, where each element is a reward
+     * @dev For example - [stable reward, trade reward 1, trade reward 2]
+     */
     function rewardOf(address lender) external view returns (uint[] memory);
 }
