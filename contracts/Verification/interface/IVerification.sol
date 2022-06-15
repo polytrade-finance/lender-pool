2c3ea1dc48577f69d9b4985cff3ea62f41eef3fb
Adam Boudj <boudjemaa.adam@gmail.com>
2022-06-15 17:57:40 +0200

    :sparkles: add natspec

--- contracts/Verification/interface/IVerification.sol
+++ contracts/Verification/interface/IVerification.sol
@@ -28,5 +28,15 @@ interface IVerification {
      */
     function isValid(address user) external view returns (bool);
 
-    function isValidationRequired(uint amount) external view returns (bool);
+    /**
+ * @notice Returns whether a validation is required or not based on deposit
+     * @dev returns a boolean if the KYC is required or not
+     * @param user, address of the user to check
+     * @param amount, amount to be added
+     * @return returns a boolean if the amount requires a Validation or not
+     */
+    function isValidationRequired(address user, uint amount)
+        external
+        view
+        returns (bool);
 }
