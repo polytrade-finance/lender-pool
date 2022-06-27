121de95b0d5cc3ac5706f54621b7f940ec8c318c
Adam Boudj <boudjemaa.adam@gmail.com>
2022-06-27 17:03:11 +0200

    :truck: reorder imports

--- contracts/RedeemPool/RedeemPool.sol
+++ contracts/RedeemPool/RedeemPool.sol
@@ -2,9 +2,9 @@
 pragma solidity =0.8.15;
 
 import "./interface/IRedeemPool.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "../Token/interface/IToken.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
  * @author Polytrade
